6346a280c8bd20879a203f0d34aaf1851b408162
해슴 <42230703+RuinaR@users.noreply.github.com>
2026-04-24 11:53:03 +0900

    상대경로 설정

--- GUICodeBuilder/tools/run_debug_windows.cmd
+++ GUICodeBuilder/tools/run_debug_windows.cmd
@@ -1,4 +1,4 @@
-@echo off
+﻿@echo off
 setlocal
 
 for %%I in ("%~dp0..") do set "ROOT=%%~fI\"
@@ -10,7 +10,17 @@ set "FLUTTER_ROOT=%ROOT%.flutter-sdk\flutter"
 if not exist "%APPDATA%" mkdir "%APPDATA%"
 if not exist "%LOCALAPPDATA%" mkdir "%LOCALAPPDATA%"
 if not exist "%PUB_CACHE%" mkdir "%PUB_CACHE%"
-
+if not exist "%ROOT%.dart_tool\package_config.json" (
+  "%ROOT%.flutter-sdk\flutter\bin\cache\dart-sdk\bin\dart.exe" ^
+    --packages="%ROOT%.flutter-sdk\flutter\packages\flutter_tools\.dart_tool\package_config.json" ^
+    "%ROOT%.flutter-sdk\flutter\bin\cache\flutter_tools.snapshot" pub get
+  if errorlevel 1 (
+    echo.
+    echo Pub get failed.
+    pause
+    exit /b 1
+  )
+)
 "%ROOT%.flutter-sdk\flutter\bin\cache\dart-sdk\bin\dart.exe" ^
   --packages="%ROOT%.flutter-sdk\flutter\packages\flutter_tools\.dart_tool\package_config.json" ^
   "%ROOT%.flutter-sdk\flutter\bin\cache\flutter_tools.snapshot" run -d windows
